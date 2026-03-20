c55f8411b8371a0ec28e94d4279fd8afab54e82b
Elena Bolt <bolt.elena@proton.me>
2026-03-20 12:53:59 +0100

    updated for Python 3.14.

--- setup_env.sh
+++ setup_env.sh
@@ -2,7 +2,7 @@
 # Creates and sets up the conda environment for the LiRI survey app
 
 ENV_NAME="liri-survey"
-PYTHON_VERSION="3.11"
+PYTHON_VERSION="3.14"
 
 echo "Creating conda environment '$ENV_NAME' with Python $PYTHON_VERSION..."
 conda create -n "$ENV_NAME" python="$PYTHON_VERSION" -y
